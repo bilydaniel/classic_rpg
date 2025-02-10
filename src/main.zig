4b8dafd8f173f26abd3aa7e80fb93c13fbbb1f79
Daniel Bily <daniel.bily@firma.seznam.cz>
2025-02-10 19:25:29 +0100

    asd

--- src/main.zig
+++ src/main.zig
@@ -9,31 +9,52 @@ const game_height: i32 = 360;
 //TODO: make resizable
 const window_width: i32 = game_width * 2;
 const window_height: i32 = game_height * 2;
+const level_width: i32 = 100;
+const level_height: i32 = 100;
+
+const tile_width: i32 = 16;
+const tile_height: i32 = 24;
 
 const Player = struct {
-    x: f32,
-    y: f32,
-    speed: f32,
+    x: i32,
+    y: i32,
+    speed: i32,
 
     pub fn init() Player {
         return Player{
-            .x = 100,
-            .y = 100,
-            .speed = 3,
+            .x = 2,
+            .y = 3,
+            .speed = 1, //TODO: speed is going to be relative to te player, player always 1
         };
     }
 };
 
+const Coordinates = struct {
+    x: i32,
+    y: i32,
+};
+
 const Level = struct {
-    grid: [][]Tile,
+    grid: [level_height][level_width]Tile, //TODO: do i want to have it always the same size?
     width: usize,
     height: usize,
+
+    pub fn init() Level {
+        return Level{
+            .width = level_width,
+            .height = level_height,
+        };
+    }
 };
 
 const Tile = struct {
     x: usize,
     y: usize,
+    occupied: bool,
+    items: []Item,
 };
+
+const Item = union(enum) {};
 
 pub fn main() !void {
     c.InitWindow(window_width, window_height, "RPG");
@@ -59,25 +80,26 @@ pub fn main() !void {
 
     while (!c.WindowShouldClose()) {
         //TODO: make a proper updating/rendering system, I think I had a thread on chatgpt or cluade
+
         if (c.IsKeyDown(c.KEY_W)) {
-            player.y -= player.speed;
+            player.y -= player.speed * tile_height;
         }
         if (c.IsKeyDown(c.KEY_S)) {
-            player.y += player.speed;
+            player.y += player.speed * tile_height;
         }
         if (c.IsKeyDown(c.KEY_A)) {
-            player.x -= player.speed;
+            player.x -= player.speed * tile_width;
         }
         if (c.IsKeyDown(c.KEY_D)) {
-            player.x += player.speed;
+            player.x += player.speed * tile_width;
         }
 
         c.BeginTextureMode(screen);
         c.ClearBackground(c.BLACK);
         //TODO: draw player
         c.DrawRectangle(
-            @as(c_int, @intFromFloat(player.x)),
-            @as(c_int, @intFromFloat(player.y)),
+            @as(c_int, @intCast(player.x)),
+            @as(c_int, @intCast(player.y)),
             16,
             16,
             c.YELLOW,
