3b8a642a087931a9995acb0eecaf8547b37bfc00
Daniel <bilidanek@seznam.cz>
2025-02-11 18:17:06 +0100

    asd

--- src/main.zig
+++ src/main.zig
@@ -78,36 +78,57 @@ pub fn main() !void {
     const offset_x = (window_width - scaled_width) / 2;
     const offset_y = (window_height - scaled_height) / 2;
 
+    const tile_texture = c.LoadTexture("assets/base_tile.png");
+    defer c.UnloadTexture(tile_texture);
+    const player_texture = c.LoadTexture("assets/random_character.png");
+    defer c.UnloadTexture(player_texture);
+
+    var timer = try std.time.Timer.start();
+    timer.reset();
+
     while (!c.WindowShouldClose()) {
         //TODO: make a proper updating/rendering system, I think I had a thread on chatgpt or cluade
 
-        if (c.IsKeyDown(c.KEY_W)) {
-            player.y -= player.speed * tile_height;
-        }
-        if (c.IsKeyDown(c.KEY_S)) {
-            player.y += player.speed * tile_height;
-        }
-        if (c.IsKeyDown(c.KEY_A)) {
-            player.x -= player.speed * tile_width;
-        }
-        if (c.IsKeyDown(c.KEY_D)) {
-            player.x += player.speed * tile_width;
+        //TODO: make a context dependent input system, ideally with a queue
+        if (timer.read() > 150_000_000) {
+            if (c.IsKeyDown(c.KEY_W)) {
+                player.y -= player.speed;
+                timer.reset();
+            }
+            if (c.IsKeyDown(c.KEY_S)) {
+                player.y += player.speed;
+                timer.reset();
+            }
+            if (c.IsKeyDown(c.KEY_A)) {
+                player.x -= player.speed;
+                timer.reset();
+            }
+            if (c.IsKeyDown(c.KEY_D)) {
+                player.x += player.speed;
+                timer.reset();
+            }
         }
 
         c.BeginTextureMode(screen);
         c.ClearBackground(c.BLACK);
         //TODO: draw player
-        c.DrawRectangle(
-            @as(c_int, @intCast(player.x)),
-            @as(c_int, @intCast(player.y)),
-            16,
-            16,
-            c.YELLOW,
-        );
+
+        // c.DrawRectangle(@as(c_int, @intCast(player.x * tile_width)), @as(c_int, @intCast(player.y * tile_height)), 16, 24, c.YELLOW);
         c.EndTextureMode();
 
         c.BeginDrawing();
         c.ClearBackground(c.BLACK);
+
+        const scaled_player_width = 16;
+        const scaled_player_height = 24;
+        c.DrawTexturePro(
+            player_texture,
+            c.Rectangle{ .x = 0, .y = 0, .width = @as(f32, @floatFromInt(player_texture.width)), .height = @as(f32, @floatFromInt(player_texture.height)) }, // Source (full texture)
+            c.Rectangle{ .x = @as(f32, @floatFromInt(player.x * tile_width)), .y = @as(f32, @floatFromInt(player.y * tile_height)), .width = @as(f32, scaled_player_width), .height = @as(f32, scaled_player_height) }, // Destination (scaled position and size)
+            c.Vector2{ .x = 0, .y = 0 }, // No origin shift
+            0.0, // No rotation
+            c.WHITE,
+        );
 
         c.DrawTexturePro(
             screen.texture,
@@ -117,6 +138,8 @@ pub fn main() !void {
             0.0,
             c.WHITE,
         );
+
+        c.DrawTexture(player_texture, @as(c_int, @intCast(player.x * tile_width)), @as(c_int, @intCast(player.y * tile_height)), c.WHITE);
         c.EndDrawing();
     }
 }
