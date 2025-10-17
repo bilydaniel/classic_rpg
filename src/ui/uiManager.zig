1a9986d6363fa93da778fb58a56b8d7d790f1565
Daniel Bily <daniel.bily@firma.seznam.cz>
2025-10-17 17:44:04 +0200

    asd

--- src/ui/uiManager.zig
+++ src/ui/uiManager.zig
@@ -1,16 +1,59 @@
 const std = @import("std");
 const Game = @import("../game/game.zig");
 const Window = @import("../game/window.zig");
+const Types = @import("../common/types.zig");
 const c = @cImport({
     @cInclude("raylib.h");
 });
 
+pub const Element = struct {
+    rect: c.Rectangle,
+    color: c.Color,
+    data: ElementData,
+
+    pub fn init(allocator: std.mem.Allocator, rect: c.Rectangle, color: c.Color) *Element {
+        const element = allocator.create(Element);
+        element.* = .{
+            .rect = rect,
+            .color = color,
+            .data = undefined,
+        };
+
+        return element;
+    }
+};
+
+pub const ElementMenuData = struct {
+    items: std.ArrayList(ElementMenuItem),
+    index: u32,
+};
+
+pub const ElementMenuItem = struct {
+    text: []u8,
+};
+
+pub const ElementData = union(ElementType) {
+    menu: ElementMenuData,
+};
+
+pub const ElementType = enum {
+    menu,
+};
+
 pub const UiManager = struct {
     allocator: std.mem.Allocator,
     ctx: *Game.Context,
+    elements: std.ArrayList(*Element),
 
     pub fn init(allocator: std.mem.Allocator, ctx: *Game.Context) !*UiManager {
         const uimanager = try allocator.create(UiManager);
+        const elements = std.ArrayList(*Element);
+
+
+
+        const deployMenu = Element.init(allocator, , color: c.Color)
+        elements.append();
+
         uimanager.* = .{
             .allocator = allocator,
             .ctx = ctx,
@@ -26,8 +69,8 @@ pub const UiManager = struct {
         const rectHeight = Window.windowHeight;
         const rectWidth = @divFloor(Window.windowWidth, 10);
 
-        _ = rectHeight;
-        _ = rectWidth;
-        //c.DrawRectangle(0, 0, rectWidth, rectHeight, c.ORANGE);
+        //_ = rectHeight;
+        //_ = rectWidth;
+        c.DrawRectangle(0, 0, rectWidth, rectHeight, c.ORANGE);
     }
 };
